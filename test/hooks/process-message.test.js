3f81fe2ca7403714a52acd58b5e7f57d0d67a638
Kris Quigley <kris@krisquigley.co.uk>
2017-12-07 20:38:31 +0000

    Initial commit

---
+++ test/hooks/process-message.test.js
@@ -0,0 +1,26 @@
+const assert = require('assert')
+const processMessage = require('../../src/hooks/process-message')
+
+describe('\'process-message\' hook', () => {
+  it('runs the hook', () => {
+    // A mock hook object
+    const mock = {
+      params: {
+        user: {
+          _id: 1
+        }
+      },
+      data: {
+        text: 'this'
+      }
+    }
+    // Initialize our hook with no options
+    const hook = processMessage()
+
+    // Run the hook function (which returns a promise)
+    // and compare the resulting hook object
+    return hook(mock).then(result => {
+      assert.equal(result, mock, 'Returns the expected hook object')
+    })
+  })
+})
